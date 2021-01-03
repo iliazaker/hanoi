3c04de0335dc5acdce7e1842a71737c764a90016
delaram <delaram.zaker@gmail.com>
2021-01-03 16:09:53 +0100

    horizontal home page

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Hanoi Towers'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
